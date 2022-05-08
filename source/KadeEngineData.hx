d0c39074ffc7713622a59df2d08bda7eb048f23a
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-08 18:48:15 -0300

    Add files via upload

--- source/KadeEngineData.hx
+++ source/KadeEngineData.hx
@@ -1,20 +1,96 @@
 import flixel.input.gamepad.FlxGamepad;
 import openfl.Lib;
 import flixel.FlxG;
+#if mobileC
+import flixel.util.FlxSave;
+import mobile.FlxVirtualPad;
+import flixel.math.FlxPoint;
+#end
 
 class KadeEngineData
 {
-    public static function initSave()
+	#if mobileC
+	// its from config file
+	var save:FlxSave;
+
+    public function new() 
     {
+        save = new FlxSave();
+    	save.bind("saveconrtol");
+    }
+
+	public function getcontrolmode():Int {
+        // load control mode num from FlxSave
+		if (save.data.buttonsmode != null) return save.data.buttonsmode[0];
+        return 0;
+    }
+
+    public function setcontrolmode(mode:Int = 0):Int {
+        // save control mode num from FlxSave
+		if (save.data.buttonsmode == null) save.data.buttonsmode = new Array();
+        save.data.buttonsmode[0] = mode;
+        save.flush();
+
+        return save.data.buttonsmode[0];
+    }
+
+    public function savecustom(_pad:FlxVirtualPad) {
+		trace("saved");
+
+		if (save.data.buttons == null)
+		{
+			save.data.buttons = new Array();
+
+			for (buttons in _pad)
+			{
+				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
+			}
+		}else
+		{
+			var tempCount:Int = 0;
+			for (buttons in _pad)
+			{
+				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
+				tempCount++;
+			}
+		}
+		save.flush();
+	}
+
+	public function loadcustom(_pad:FlxVirtualPad):FlxVirtualPad {
+		//load pad
+		if (save.data.buttons == null) return _pad;
+		var tempCount:Int = 0;
+
+		for(buttons in _pad)
+		{
+			buttons.x = save.data.buttons[tempCount].x;
+			buttons.y = save.data.buttons[tempCount].y;
+			tempCount++;
+		}	
+        return _pad;
+	}
+	#end
+
+	public static function initSave()
+	{
         if (FlxG.save.data.newInput == null)
 			FlxG.save.data.newInput = true;
 
 		if (FlxG.save.data.downscroll == null)
 			FlxG.save.data.downscroll = false;
 
+		if (FlxG.save.data.middlescroll == null)
+			FlxG.save.data.middlescroll = false;
+
 		if (FlxG.save.data.dfjk == null)
 			FlxG.save.data.dfjk = false;
-			
+
+		#if mobileC
+		if (FlxG.save.data.fastValue == null)
+			FlxG.save.data.fastValue = 0;
+		#end
+
 		if (FlxG.save.data.accuracyDisplay == null)
 			FlxG.save.data.accuracyDisplay = true;
 
@@ -24,8 +100,10 @@ class KadeEngineData
 		if (FlxG.save.data.songPosition == null)
 			FlxG.save.data.songPosition = false;
 
-		if (FlxG.save.data.fps == null)
-			FlxG.save.data.fps = false;
+		#if desktop
+		if (FlxG.save.data.inputShow == null)
+			FlxG.save.data.inputShow = false;
+		#end
 
 		if (FlxG.save.data.changedHit == null)
 		{
@@ -34,15 +112,27 @@ class KadeEngineData
 			FlxG.save.data.changedHit = false;
 		}
 
-		if (FlxG.save.data.fpsRain == null)
-			FlxG.save.data.fpsRain = false;
+		#if mobileC
+		if (FlxG.save.data.fpsCap == null)
+			FlxG.save.data.fpsCap = 60;
+
+		if (FlxG.save.data.fpsCap > 90)
+			FlxG.save.data.fpsCap = 60; // baby proof so you can't hard lock ur copy of kade engine
+		#else
 
 		if (FlxG.save.data.fpsCap == null)
 			FlxG.save.data.fpsCap = 120;
 
 		if (FlxG.save.data.fpsCap > 285 || FlxG.save.data.fpsCap < 60)
 			FlxG.save.data.fpsCap = 120; // baby proof so you can't hard lock ur copy of kade engine
-		
+		#end
+
+		if (FlxG.save.data.fps == null)
+			FlxG.save.data.fps = false;
+
+		if (FlxG.save.data.fpsRain == null)
+			FlxG.save.data.fpsRain = false;
+
 		if (FlxG.save.data.scrollSpeed == null)
 			FlxG.save.data.scrollSpeed = 1;
 
@@ -69,7 +159,7 @@ class KadeEngineData
 
 		if (FlxG.save.data.resetButton == null)
 			FlxG.save.data.resetButton = false;
-		
+
 		if (FlxG.save.data.botplay == null)
 			FlxG.save.data.botplay = false;
 
@@ -78,7 +168,7 @@ class KadeEngineData
 
 		if (FlxG.save.data.strumline == null)
 			FlxG.save.data.strumline = false;
-		
+
 		if (FlxG.save.data.customStrumLine == null)
 			FlxG.save.data.customStrumLine = 0;
 
@@ -91,11 +181,20 @@ class KadeEngineData
 		if (FlxG.save.data.inputShow == null)
 			FlxG.save.data.inputShow = false;
 
-		if (FlxG.save.data.optimize == null)
-			FlxG.save.data.optimize = false;
-		
+		/*if (FlxG.save.data.optimize == null)
+			FlxG.save.data.optimize = false;*/
+
+		if (FlxG.save.data.char == null)
+			FlxG.save.data.char = true;
+
+		if (FlxG.save.data.bg == null)
+			FlxG.save.data.bg = true;
+
+		if (FlxG.save.data.freeplayMusic == null)
+			FlxG.save.data.freeplayMusic = false;
+
 		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
-		
+
 		KeyBinds.gamepad = gamepad != null;
 
 		Conductor.recalculateTimings();
