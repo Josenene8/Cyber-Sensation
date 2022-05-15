1c66a92cb65e27699fe18758661ac7d0f140c375
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-15 14:26:42 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -137,24 +137,6 @@ class AndroidControls extends Option
 		return "Android Controls";
 	}
 }
-class CustomControls extends Option
-{
-	public function new(desc:String)
-	{
-		super();
-		description = desc;
-	}
-	public override function press():Bool
-	{
-		FlxG.switchState(new options.CustomControlsState());
-		return true;
-	}
-	private override function updateDisplay():String
-	{
-		return "controls";
-	}
-
-}
 
 class DownscrollOption extends Option
 {
